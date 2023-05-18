2c1105112fc632bbf53e43fb26230a1d0bbc63b7
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-05-18 20:46:11 +0300

    Component Done!

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-const submitButton = document.querySelector('#submit')
+// Get references to the necessary DOM elements
+const submitButton = document.querySelector('#submit');
+const ratingInputs = document.querySelectorAll('input[name="rating"]');
+const finalScore = document.querySelector('.final-score');
+const modal = document.querySelector('.modal');
+const body = document.body;
+const container = document.querySelector('.container')
+
+// Hide the modal by default
+modal.style.visibility = 'hidden';
+
+// Add event listener to the submit button
+submitButton.addEventListener('click', (e) => {
+  // Get the value of the selected rating
+   const selectedRating = document.querySelector('[name="rating"]:checked');
+
+  // Check if a rating is selected
+  if (!selectedRating) {
+    e.preventDefault(); // Prevent form submission
+    return;
+  }
+  
+  container.style.display = 'none';
+  // Show the modal
+  modal.style.visibility = 'visible';
+  
+  // Display the selected rating in the modal
+  finalScore.textContent = `You selected ${selectedRating.value} out of 5`;
+  
+
+});
+
+// Add event listener to the window's click event
+
